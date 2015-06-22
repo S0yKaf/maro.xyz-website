05ab9bdd41e9da1dd3e6d8609aafbdf13034959c
Justin Duplessis <drfoliberg@gmail.com>
2015-06-21 20:42:42 -0400

    wip file upload

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 
 angular.module('myblt', [
   'ngRoute',
+  'ngFileUpload',
   'app.controllers.Index'
 ]).config(['$routeProvider', '$httpProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'Index'});
--- public/controllers/index.js
+++ public/controllers/index.js
@@ -1,5 +1,16 @@
-var controllers = angular.module('app.controllers.Index', []);
+var controllers = angular.module('app.controllers.Index', ['ngFileUpload']);
 
-controllers.controller('Index', function($scope) {
-    $scope.test = "hello"
+controllers.controller('Index', function($scope, Upload) {
+
+    $scope.upload = function(file) {
+        Upload.upload({
+            url: '/api/upload',
+            file: file
+        }).progress(function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+        }).success(function (data, status, headers, config) {
+            console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+        });
+    }
 });
