acee71066c433ad09fac7958bdb85ac2988f9f02
Justin Duplessis <drfoliberg@gmail.com>
2016-02-05 23:18:14 -0500

    OK

--- myblt.py
+++ myblt.py
@@ -222,7 +222,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 2:
         conf = sys.argv[1]
-        print('loading additional config ' + conf)
+        print('Loading additional config %s...', conf)
         app.config.from_pyfile('config/' + conf + '_config.py')
 
     init_engine(app.config['DATABASE_URI'])
