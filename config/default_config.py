c821fa34acef3b6860f3cb16cca6151eefdbc57f
Kiniamaro <vdyotte@gmail.com>
2016-04-05 12:05:36 -0400

    stop tracking configs

--- config/default_config.py
+++
@@ -1,6 +0,0 @@
-DEBUG = False
-UPLOAD_FOLDER = '/home/kiniamaro/maro.xyz/uploads'
-API_URL = 'http://a.maro.xyz/'
-IS_PRIVATE = False
-DOUBLE_EXTS = ['tar']
-DATABASE_URI = 'sqlite:////home/kiniamaro/maro.xyz/test.db'
